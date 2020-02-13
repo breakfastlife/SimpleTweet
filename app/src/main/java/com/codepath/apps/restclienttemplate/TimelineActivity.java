2abdcb5a4bad1bdf948e4246d99c9de402b629db
Bryce Patrick Ellard <bpe2@humboldt.edu>
2020-02-13 12:35:44 -0800

    All

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,42 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
+public class TimelineActivity extends AppCompatActivity {
+    public final String TAG = "TimelineActivity";
+
+    TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        Log.i(TAG, "building...");
+        client = TwitterApplication.getRestClient(this);
+        Log.i(TAG, "built");
+        Log.i(TAG, "Populating...");
+        populateHomeTimeline();
+        Log.i(TAG, "Populated");
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "onFailure", throwable);
+            }
+        });
+    }
+}
